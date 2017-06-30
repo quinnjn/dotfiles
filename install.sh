46f1dfc868febfbf1e4d7b292be40b5695f1d996
quinnjn <quinn@neumiiller.com>
2017-06-30 05:49:49 -0700

    Reorganize dotfiles

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# symlink dotfiles
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/gitconfig ~/.gitconfig
+
+# create .bin directory
+# rm -rf ~/.bin
+# mkdir ~/.bin
+# cp -r bin/* ~/.bin
+
+# setup vim
+# rm -rf ~/.vim
+# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# vim +PluginInstall +qall
