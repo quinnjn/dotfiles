6ede85da5f865a2ff16e78331cc256f981e823c5
quinnjn <quinn@neumiiller.com>
2019-05-10 11:14:38 -0700

    Add sdkman profile

---
+++ bash/profile/sdkman.sh
@@ -0,0 +1,3 @@
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/Users/qneumiiller/.sdkman"
+[[ -s "/Users/qneumiiller/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/qneumiiller/.sdkman/bin/sdkman-init.sh"
