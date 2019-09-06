6c94c7833a922883e2b5d0e0af2fe7e36957b8ac
quinnjn <quinn@neumiiller.com>
2019-09-06 11:36:15 -0700

    dotfiles alias

--- bash/aliases/all.sh
+++ bash/aliases/all.sh
@@ -1,5 +1,6 @@
 alias vi=vim
 alias dev="cd ~/git/github.com/quinnjn"
+alias dotfiles="cd ~/git/github.com/quinnjn/dotfiles"
 alias youtube-dl-mp3='youtube-dl --extract-audio --audio-format mp3 --metadata-from-title "%(artist)s - %(title)s" --add-metadata --audio-quality 0 -v'
 
 # Reload bash
