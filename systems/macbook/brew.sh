39beaae0e3346dee1325fdb8c39bc3e62925ff3c
quinnjn <quinn@neumiiller.com>
2020-12-15 11:02:42 -0800

    Store installed casks/formula when brew installed

--- systems/macbook/brew.sh
+++ systems/macbook/brew.sh
@@ -7,9 +7,11 @@ set -o pipefail
 brew_installed=$(type brew &> /dev/null)
 
 if [ brew_installed ]; then
-  brew upgrade
-  brew cask upgrade
+  echo "Brew not installed."
+  exit
 fi
 
+brew upgrade
+brew cask upgrade
 brew list --formula > brew-formulas
 brew list --cask > brew-casks
