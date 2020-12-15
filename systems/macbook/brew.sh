11f339d7733bf91de3a7e110ec90c0b4e46c2ae1
quinnjn <quinn@neumiiller.com>
2020-12-15 13:49:54 -0800

    Update deprecated brew cask upgrade command

--- systems/macbook/brew.sh
+++ systems/macbook/brew.sh
@@ -6,12 +6,12 @@ set -o pipefail
 
 brew_installed=$(type brew &> /dev/null)
 
-if [ brew_installed ]; then
+if [ ! brew_installed ]; then
   echo "Brew not installed."
   exit
 fi
 
 brew upgrade
-brew cask upgrade
+brew upgrade --cask
 brew list --formula > brew-formulas
 brew list --cask > brew-casks
