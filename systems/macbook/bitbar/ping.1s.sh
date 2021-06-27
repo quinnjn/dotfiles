53cf2c52ae277080d4e9703eae5aac4ee1fdd067
quinnjn <quinn@neumiiller.com>
2021-06-27 16:16:06 -0700

    Run pypl every 15m

--- systems/macbook/bitbar/ping.1s.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# <bitbar.title>PING</bitbar.title>
-# <bitbar.version>0.0.0</bitbar.version>
-# <bitbar.author>quinnjn</bitbar.author>
-# <bitbar.author.github>quinnjn</bitbar.author.github>
-# <bitbar.desc>Pings 8.8.8.8</bitbar.desc>
-
-response=$(ping -c 1 8.8.8.8 -nQ | head -n 2 | tail -n 1 | awk '{ split($7, a, "="); print a[2] }')
-echo "📡 $response"
