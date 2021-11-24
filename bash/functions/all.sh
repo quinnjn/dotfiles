77f353d382ebfef4cc726676526a0609a25cddd4
quinnjn <quinn@neumiiller.com>
2021-11-24 14:24:17 -0800

    is-process-running

--- bash/functions/all.sh
+++ bash/functions/all.sh
@@ -5,3 +5,16 @@ template() {
 
   cp ~/.templates/$1 $2
 }
+
+# Checks if a process is running
+is-process-running () {
+  keywords=$@
+  pscount=$(ps aux | grep -E "${keywords// /|}" | wc -l)
+  echo $pscount
+
+  if [ $pscount -gt 1 ]; then
+    return 0
+  fi
+
+  return 1
+}
