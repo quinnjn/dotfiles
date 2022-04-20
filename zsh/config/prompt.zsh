72519cf25d104f6cf05228b9917e614795973a48
quinnjn <quinn@neumiiller.com>
2022-04-20 10:05:33 -0400

    Prompt the ZSH way

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -1,58 +1,37 @@
-git_prompt() {
-  local s=""
-  if ! git rev-parse --is-inside-work-tree >/dev/null; then
-    # Not in a git repo
-    return
-  fi
+# vcs_info gives us version control status
+autoload -Uz vcs_info
 
-  if ! git rev-parse --is-inside-git-dir >/dev/null; then
-    # Not in a git dir
-    return
-  fi
+# Enable git
+zstyle ':vcs_info:*' enable git
 
-  # check for uncommitted changes in the index
-  if ! $(git diff --quiet --ignore-submodules --cached); then
-    s+="+";
-  fi
-
-  # check for unstaged changes
-  if ! $(git diff-files --quiet --ignore-submodules --); then
-    s+="!";
-  fi
-
-  # check for untracked files
-  if [ -n "$(git ls-files --others --exclude-standard)" ]; then
-    s+="?";
-  fi
-
-  # check for stashed files
-  if $(git rev-parse --verify refs/stash &>/dev/null); then
-    s+="$";
-  fi
-
-  local branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
-    git rev-parse --short HEAD 2> /dev/null || \
-    printf "(unknown)")"
-
-  printf "%s" " on %{$fg[blue]%}${branchName} [$s]%{$reset_color%}"
+# Executed before each prompt
+precmd() {
+  # Update vcs info
+  vcs_info
 }
+
+# Allow parameter expansion
+setopt prompt_subst
+# Asks vcs change status
+zstyle ':vcs_info:git*' check-for-changes true
+zstyle ':vcs_info:git*' formats " on %F{blue}%b [%m%u%c]%f"
 
 prompt() {
   local prompt=""
 
   # Display current user
-  prompt+="%{$fg[red]%}%n%{$reset_color%}"
+  prompt+="%{%F{red}%}%n%f"
 
   # Display current machine
   prompt+=" at "
-  prompt+="%{$fg[yellow]%}%m%{$reset_color%}"
+  prompt+="%{%F{yellow}%}%m%f"
 
   # Display working directory
   prompt+=" in "
-  prompt+="%{$fg[green]%}%~%{$reset_color%}"
+  prompt+="%{%F{green}%}%~%f"
 
-  # TODO Display git details
-  # prompt+="$(git_prompt)"
+  # Display git details
+  prompt+='${vcs_info_msg_0_}'
 
   prompt+="\n"
   prompt+="$ "
@@ -60,4 +39,4 @@ prompt() {
   echo $prompt
 }
 
-PS1=$(prompt)
+PROMPT=$(prompt)
