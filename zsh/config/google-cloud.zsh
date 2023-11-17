b6e29a7b6756d886de25f477a4ce3c7ffb924780
quinnjn <quinn@neumiiller.com>
2023-11-17 11:50:44 -0800

    Revert "devcontainer: dont setup gcloud if brew not available"
    This reverts commit 2aa6fa536a271acd9db71cc45820b84b180b794f.

--- zsh/config/google-cloud.zsh
+++ zsh/config/google-cloud.zsh
@@ -1,8 +1,3 @@
-if [[ "$(type -p brew)" == "brew not found" ]]; then
-  # brew is not installed. Skip this section.
-  return
-fi
-
 if [[ -f "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc" ]]; then
   source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
 fi
