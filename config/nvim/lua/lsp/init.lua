a8bf651ba22db13b5c9180e977a4cce46122e865
quinnjn <quinn@neumiiller.com>
2024-05-10 13:50:37 -0700

    upgrade neovim

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -10,6 +10,8 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
+vim.lsp.set_log_level('debug')
+
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
@@ -33,8 +35,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
+
+    vim.keymap.set('n', '<Leader>f', function()
       vim.lsp.buf.format { async = true }
+    end, opts)
+
+    vim.keymap.set('n', '<Leader>ft', function()
+      vim.lsp.buf.format()
     end, opts)
   end,
 })
--- zsh/functions/nvim-test.zsh
+++ zsh/functions/nvim-test.zsh
@@ -1,4 +1,4 @@
 # Test nvim with config changes before applying them
 nvim-test() {
-  XDG_CONFIG_HOME=~/git/github.com/quinnjn/dotfiles/config nvim
+  XDG_CONFIG_HOME=~/git/github.com/quinnjn/dotfiles/config nvim "$@"
 }
