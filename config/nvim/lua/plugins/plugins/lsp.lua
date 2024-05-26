1d73875d2347476d70145bd0e67fe0d8d8b8cbc2
quinnjn <quinn@neumiiller.com>
2024-05-26 14:44:52 -0700

    use pattern for eslint fix all

--- config/nvim/lua/plugins/plugins/lsp.lua
+++ config/nvim/lua/plugins/plugins/lsp.lua
@@ -14,7 +14,12 @@ return {
       lspconfig.eslint.setup({
         on_attach = function(client, bufnr)
           vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
+            pattern = {
+              "*.js",
+              "*.ts",
+              "*.graphql",
+            },
+            -- buffer = bufnr,
             command = "EslintFixAll",
           })
         end,
