a1cf09e0fd21293ad3d5dccfc87f42f3eefc26d9
quinnjn <quinn@neumiiller.com>
2024-06-21 09:51:15 -0700

    use eslint over eslint_d

--- config/nvim/lua/plugins/plugins/linting.lua
+++ config/nvim/lua/plugins/plugins/linting.lua
@@ -6,8 +6,8 @@ return {
       local lint = require("lint")
 
       lint.linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
+        javascript = { "eslint" },
+        typescript = { "eslint" },
       }
 
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
