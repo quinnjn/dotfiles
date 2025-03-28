6d79df1784e9f2a8f8d7925047b5f5e96cec3249
quinnjn <quinn@neumiiller.com>
2025-03-28 16:08:07 -0700

    setup: update dock settings

--- systems/macbook/defaults.sh
+++ systems/macbook/defaults.sh
@@ -14,8 +14,24 @@ defaults write com.apple.screencapture location -string "$HOME/Desktop"
 # Screenshots should be PNGs.
 defaults write com.apple.screencapture type -string "png"
 
+# Dock settings
+
+# Hide the dock
+defaults write com.apple.dock autohide -bool true 
+# Move the dock to the left
+defaults write com.apple.dock orientation -string left 
+# Remove apps from the Dock
+defaults write com.apple.dock persistent-apps -array
+# Set the dock to a certain tile size
+defaults write com.apple.dock tilesize -int 36
+# Restart the dock
+killall Dock
+
 # Restart
 # -------
 
 # Restart SystemUIServer.
 killall SystemUIServer
+
+# Make the default system appearance "Dark"
+osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
